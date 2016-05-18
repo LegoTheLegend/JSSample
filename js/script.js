1b89f0b3bdcb81859dc864e067563905927df9c7
LegoTheLegend <jlllego@gmail.com>
2016-05-18 16:25:18 +0300

    Done from 1 till 5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
+var userChoice = prompt('Will you really read this?');
+if (userChoice = 'yes') {
+    console.log('Allright then');
+}
+else {
+    console.log('Allright anyway');
+}
 
-var myVar = 1;
+//HomeWork 1;
+alert('I am Javascript');
+
+//HomeWork 2;
+var admin, name;
+name = 'Vasiliy';
+admin = name;
+console.log (admin);
+
+//HomeWork 3;
+var planetName = 'Earth';
+var userName = 'Peter';
