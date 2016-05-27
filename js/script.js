06eb31027ac72c1d95099653276e5f86f2e56722
LegoTheLegend <jlllego@gmail.com>
2016-05-27 17:24:40 +0300

    homework

--- js/script.js
+++ js/script.js
@@ -19,3 +19,66 @@ console.log (admin);
 //HomeWork 3;
 var planetName = 'Earth';
 var userName = 'Peter';
+
+//HomeWork 4;
+var a = 2;
+var x = 1 + (a *= 2);
+x = 5;
+
+//HomeWork 5;
+var userLogin = prompt('Your name?');
+alert(userLogin);
+
+//HomeWork 6;
+if ("0") {
+    alert( 'Привет' ); //Yes;
+}
+
+var userNubmer = +prompt('enter the number');
+if (userNumber > 0) {
+    console.log('1');
+}
+else if (userNumber < 0) {
+    console.log('-1');
+}
+else {
+    console.log('0');
+}
+
+var userLoginfo = prompt('Who are you?');
+if (userLoginfo == 'Admin') {
+    var pass = prompt('Password?');
+    if (pass == 'Correct') {
+        alert('Welcome');
+    } else if (pass == null) {
+        alert('Access denied');
+    } else {
+        alert('Wrong password');
+    }
+} else if (userName == null) {
+    alert('Access denied');
+} else {
+    alert('You are not registered');
+}
+
+result (a + b < 4) ? 'less' : 'more';
+
+var message = (login == 'Ivan') ? 'Greetings' :
+    (login == 'CEO') ? 'Welcome' :
+        (login == '') ? 'No login info' :
+            '';
+
+//HomeWork 7;
+2;
+
+alert(1); 2;
+
+alert(1);
+
+3;
+
+if (userAge >= 14 && userAge < 90);
+
+if (userAge < 14 || userAge > 90);
+
+1st and 3rd;
